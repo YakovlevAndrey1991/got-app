d48135980f025e5e8c1e53542b23f79c40496b8d
Andrey Yakovlev <ultima.pank@gmail.com>
2021-05-23 21:12:49 +0300

    add spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './spinner.css';
+
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-pulse-y6dory1esi">
+            <div className="ldio-3was27gkwkk">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+export default Spinner;
+
+
