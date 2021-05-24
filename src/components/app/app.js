808e658d4006f779fcd1363622f8d1440ea22df8
Yakovlev Andrey <yakovlev@msk.radarmaster.ru>
2021-05-24 12:36:32 +0300

    inicial commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,6 +11,7 @@ const App = () => {
         <> 
             <Container>
                 <Header />
+                <Header />
             </Container>
             <Container>
                 <Row>
